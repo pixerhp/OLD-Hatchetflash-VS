aa1358532deb0d13df35c4984cd3f9ba0dc72f38
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2022-09-05 18:47:33 -0400

    Made a nice window and added a folder
    I made the program create a window which is:
    - automatically the size of your monitor,
    - automatically maximized.
    
    Additionally, I added a folder for any text documents I'd like to write, whether it be a helpful thing for all of the devs to see, or a random thing I only need to remember for a moment.
    
    (I also made it so that VS recognizes where the glad and glfw files are when in release mode rather than debug mode.)

--- Main.cpp
+++ Main.cpp
@@ -4,5 +4,39 @@
 
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+	const GLFWvidmode * monitorVideoMode = glfwGetVideoMode(primaryMonitor);
+	int monitorWidth = monitorVideoMode->width; int monitorHeight = monitorVideoMode->height;
+
+	GLFWwindow* window = glfwCreateWindow(monitorWidth, monitorHeight, "Hatchetflash - Pre-Alpha", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMaximizeWindow(window);
+	glfwMakeContextCurrent(window);
+	
+	gladLoadGL();
+	glViewport(0, 0, monitorWidth, monitorHeight); //NOT SURE IF WIDTH AND HEIGHT NEED TO BE SWAPPED.
+
+	glClearColor(0.08f, 0.14f, 0.20f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
