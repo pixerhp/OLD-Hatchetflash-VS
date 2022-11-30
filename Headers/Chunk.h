89db30b4a88ecad9c32904cb54cb1738dbef6813
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2022-11-30 18:55:09 -0500

    Revised Chunk.h to be more what I want it to be.
    The program, despite now being broken, has begun the process of reforming what I want chunk functionality and texture loading (and related to be) to be. I've started here by updating Chunk.h to much better fit what the newer design plan is.

--- Headers/Chunk.h
+++ Headers/Chunk.h
@@ -11,28 +11,61 @@
 #include "Mesh.h"
 
 
-class Chunk {
-private:
-	Mesh chunkMesh; //(The chunk's mesh which is what's rendered; can be updated using certain functions.)
+class Chunk
+{
+	private:
+		Mesh chunkMesh; //(The chunk's mesh which is what's rendered; can be updated using certain functions.)
 
-public:
-	std::map<int, int> ThingIDmap;
-	int ThingIDsize;
-	unsigned long int seed = 314;
-	int chunkX = 0;
-	int chunkY = 0;
-	int chunkZ = 0;
-	std::vector<unsigned int> blockStorage[4096]; //(Contains an array of 4096 (16^3) unsigned int vectors, which can allow for each array slot (block position in a chunk) to contain multiple actual block thingos.
 
-	// The constructor for chunk objects. (Unrelated to how they we be after worldgen stuff, this is just when the raw object is created.)
-	Chunk(unsigned long int input_seed, int input_chunkX, int input_chunkY, int input_chunkZ, std::map<int, int> input_ThingIDmap, int input_ThingIDsize) : blockStorage() {
-		seed = input_seed;
-		chunkX = input_chunkX;
-		chunkY = input_chunkY;
-		chunkZ = input_chunkZ;
-		ThingIDmap = input_ThingIDmap;
-		ThingIDsize = input_ThingIDsize;
-	}
+	public:
+		// A list of the variables each chunk object independantly contains:
+		uint32_t chunkWorldgenSeed = 314; //(The default chunk object's seed being 314 is an arbitrary choice, it can be any number.)
+		uint8_t chunkPiome = 0;
+		uint16_t chunkBiome = 0;
+		uint8_t chunkChangesCount = 0; //Stores the number of times the blocks or content that the chunk is filled with have been changed whilst it's been loaded.
+		int32_t chunkCoordsX = 0;
+		int32_t chunkCoordsY = 0;
+		int32_t chunkCoordsZ = 0;
+// NOTE TO SELF FOR THE FUTURE: CHANGE THE DEFAULT CHUNK MATERIAL TO AIR INSTEAD OF NOTHING LATER WHEN YOU CAN!
+		std::vector<uint8_t> defaultChunkMaterial{}; //Used for defining the chunk is defaultly filled with, usually air or water. it's a vector of single byte ints since the material may have extra info.
+		std::vector<std::vector<uint8_t>> chunkBlockStorage{}; //A vector of vectors of single bytes, used for storing a list of all of the blocks within the chunk.
+		
+
+		// A selection of different chunk-object constructors. (It's unrelated to world generation, this is just for when the raw class object itself is created.)
+		Chunk() {
+		}
+		Chunk(uint32_t inputChunkWorldgenSeed, int32_t inputChunkCoordsX, int32_t inputChunkCoordsY, int32_t inputChunkCoordsZ) {
+			chunkWorldgenSeed = inputChunkWorldgenSeed;
+			chunkCoordsX = inputChunkCoordsX;
+			chunkCoordsY = inputChunkCoordsY;
+			chunkCoordsZ = inputChunkCoordsZ;
+		}
+		Chunk(uint32_t inputChunkWorldgenSeed, int32_t inputChunkCoordsX, int32_t inputChunkCoordsY, int32_t inputChunkCoordsZ, std::vector<uint8_t> inputDefaultChunkMaterial, std::vector<std::vector<uint8_t>> inputChunkBlockStorage) {
+			chunkWorldgenSeed = inputChunkWorldgenSeed;
+			chunkCoordsX = inputChunkCoordsX;
+			chunkCoordsY = inputChunkCoordsY;
+			chunkCoordsZ = inputChunkCoordsZ;
+			defaultChunkMaterial = inputDefaultChunkMaterial;
+			chunkBlockStorage = inputChunkBlockStorage;
+		}
+		Chunk(uint32_t inputChunkWorldgenSeed, uint8_t inputChunkPiome, uint16_t inputChunkBiome, int32_t inputChunkCoordsX, int32_t inputChunkCoordsY, int32_t inputChunkCoordsZ) {
+			chunkWorldgenSeed = inputChunkWorldgenSeed;
+			chunkPiome = inputChunkPiome;
+			chunkBiome = inputChunkBiome;
+			chunkCoordsX = inputChunkCoordsX;
+			chunkCoordsY = inputChunkCoordsY;
+			chunkCoordsZ = inputChunkCoordsZ;
+		}
+		Chunk(uint32_t inputChunkWorldgenSeed, uint8_t inputChunkPiome, uint16_t inputChunkBiome, int32_t inputChunkCoordsX, int32_t inputChunkCoordsY, int32_t inputChunkCoordsZ, std::vector<uint8_t> inputDefaultChunkMaterial, std::vector<std::vector<uint8_t>> inputChunkBlockStorage) {
+			chunkWorldgenSeed = inputChunkWorldgenSeed;
+			chunkPiome = inputChunkPiome;
+			chunkBiome = inputChunkBiome;
+			chunkCoordsX = inputChunkCoordsX;
+			chunkCoordsY = inputChunkCoordsY;
+			chunkCoordsZ = inputChunkCoordsZ;
+			defaultChunkMaterial = inputDefaultChunkMaterial;
+			chunkBlockStorage = inputChunkBlockStorage;
+		}
 
 	void Draw(); //(Used to initiate rendering the chunk's mesh, which is actually stored in "chunkMesh" and can be updated by certain functions.)
 	
